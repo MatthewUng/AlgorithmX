6379f6d1458e881bdd708e382777fcd73de4c47d
Matthew Ung <matthew.ung814@gmail.com>
2016-12-30 23:51:07 -0800

    working on creating matrix

--- matrix.java
+++ matrix.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
  */
 class Matrix{
     private Node mainnode = new Node();
+    public int w, h;
 
     Matrix(){
 
@@ -42,6 +43,15 @@ class Matrix{
             rowheader = row;
         }
 
+        void insertbelow(Node n){
+            n.below = this.below;
+            n.above = this;
+            n.rowheader = rowheader;
+
+            below.above = n;
+            below = n;
+        }
+
         void deleteVertical(){
             above.below = below;
             below.above = above;
@@ -77,6 +87,10 @@ class Matrix{
      * creates the sparse matrix with the given inputs
      */
     void createMatrix(int w, int h, LinkedList<Coord> list){
+
+        this.w = w;
+        this.h = h;
+
         //creating the col headers
         Node temp = mainnode;
         for(int i = 1; i <= w; i++){
@@ -180,6 +194,45 @@ class Matrix{
         }
     }
 
+    /*
+     * prints out the matrix represented
+     */
+    void print(){
+        System.out.println("Height of: "+h);
+        System.out.println("Width of: "+w);
+
+        //iterating through columns
+        Node it = mainnode;
+        while(it.right != mainnode){
+            System.out.print("Column: "+it.right.rowcolnum);
+
+            //iterate through specific column
+            Node colit = it.right;
+            while(colit.below != it.right){
+                System.out.println("("+colit.below.rowheader.rowcolnum+", "+
+                colit.below.colheader.rowcolnum+")");
+
+                colit = colit.below;
+            }
+            it = it.right;
+        }
+
+        //iterating through rows
+        it = mainnode;
+        while(it.below != mainnode){
+            System.out.println("Row: "+it.right.rowcolnum);
+
+            Node rowit = it.below;
+            while(rowit.right != it.below){
+                System.out.println("("+rowit.right.rowheader.rowcolnum+", "+
+                rowit.below.rowheader.rowcolnum+")");
+
+                rowit = rowit.below;
+            }
+            it = it.below;
+        }
+   }
+
     public static void main(String [] args){
         System.out.println("testing...");
         // 6 x 7 
@@ -201,5 +254,7 @@ class Matrix{
         values.add(new Coord(6,7));
         Matrix m = new Matrix();
         
+        m.createMatrix(7, 6, values);
+        m.print();
     }
 }
